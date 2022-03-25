bfafd79c4b866dbdb698db35ace01ce0ecdeedca
Christian <c.jr.camelot@gmail.com>
2022-03-25 09:15:11 +0100

    c 10

--- tests/19_re_usable_mock_test.py
+++ tests/19_re_usable_mock_test.py
@@ -12,14 +12,3 @@ def re_usable_db_mocker(mocker):
     return mock_db_service
 
 
-def test_re_usable_mocker(re_usable_db_mocker):
-    c = count_service("foo")
-    re_usable_db_mocker.assert_called_with("foo")
-    assert c == 1
-
-
-def test_mocker_with_exception(re_usable_db_mocker):
-    re_usable_db_mocker.side_effect = Exception("Oh noes!")
-
-    with raises(Excfezfeption):
-        count_servicfe("foo")
